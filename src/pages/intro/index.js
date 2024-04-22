fdb71d85f862979b9d7b41185144d224fee2fb6b
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2024-04-22 17:53:51 +0800

    include curator and instructor

--- src/pages/intro/index.js
+++ src/pages/intro/index.js
@@ -77,6 +77,28 @@ export const Introduction = () => {
             </div>
           </Col>
         </Row>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <hr className="t_border my-4 ml-0 text-left" />
+            <p>
+              <div>
+                <strong>Curator:</strong> Sarah Perlman, Isabelle, Claire Hou, Calvin
+                Lin, Yao Xiao
+              </div>
+              <div>
+                <strong>Instructor:</strong> Professor{" "}
+                <a
+                  href="https://shanghai.nyu.edu/academics/faculty/directory/lala-zuo"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  LaLa Zuo
+                </a>
+              </div>
+              <div>April 2022</div>
+            </p>
+          </Col>
+        </Row>
         <div className="intro_btn-action pb-5">
           <Link to="/" className="text_2">
             <div id="button_p" className="ac_btn btn">
